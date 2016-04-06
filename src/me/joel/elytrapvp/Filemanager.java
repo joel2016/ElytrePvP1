2325bfb262b31fec9d4dca3e23d3449c340989b8
Jan Bukkit <info@mine-home.net>
2016-04-07 01:12:44 +0200

    Commands in eingenen Ordner verschoben

--- src/me/joel/elytrapvp/Filemanager.java
+++ src/me/joel/elytrapvp/Filemanager.java
@@ -8,26 +8,22 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Filemanager {
 
-	  public static File file = new File("plugins/" + Main.main.getName(), "config.yml");
-	  public static FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+	public static File file = new File("plugins/" + Main.main.getName(), "config.yml");
+	public static FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 
-	  public void register() {
-	    cfg.options().copyDefaults(true);
-	    cfg.addDefault("Minplayers", Integer.valueOf(Main.main.max));
-	    cfg.addDefault("Minplayers", Integer.valueOf(Main.main.min));
-	    savecfg();
-	    Main.main.max = cfg.getInt("");
-	    Main.main.max = cfg.getInt("");
-	  }
+	public void register() {
+		cfg.options().copyDefaults(true);
+		cfg.addDefault("Minplayers", Integer.valueOf(Main.main.max));
+		cfg.addDefault("Minplayers", Integer.valueOf(Main.main.min));
+		savecfg();
+		Main.main.max = cfg.getInt("");
+		Main.main.max = cfg.getInt("");
+	}
 
-	  public static void savecfg()
-	  {
-	    try
-	    {
-	      cfg.save(file);
-	    }
-	    catch (IOException localIOException)
-	    {
-	    }
-	  }
+	public static void savecfg() {
+		try {
+			cfg.save(file);
+		} catch (IOException localIOException) {
+		}
+	}
 }
--- src/me/joel/elytrapvp/Main.java
+++ src/me/joel/elytrapvp/Main.java
@@ -5,23 +5,24 @@ import java.util.ArrayList;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
-	
-		   public int min = 2;
-		   public int max = 8;
-		   public Filemanager fm;
-		   public static ArrayList<Player> game = new ArrayList();
-		   public static Main main;
+import me.joel.elytrapvp.commands.Commands;
 
-		   public void onEnable()
-		   {
-		     System.out.println("[ElytrenPvP] Das Plugin wurde aktivirt.");
-		     game = new ArrayList();
-		     main = this;
-		     this.fm = new Filemanager();
-		     Filemanager.savecfg();
-		     this.fm.register();
-		     getCommand("help").setExecutor(new Commands());
-		   }
+public class Main extends JavaPlugin {
+
+	public int min = 2;
+	public int max = 8;
+	public Filemanager fm;
+	public static ArrayList<Player> game = new ArrayList();
+	public static Main main;
+
+	public void onEnable() {
+		System.out.println("[ElytraPvP] Das Plugin wurde aktivirt.");
+		game = new ArrayList();
+		main = this;
+		this.fm = new Filemanager();
+		Filemanager.savecfg();
+		this.fm.register();
+		getCommand("help").setExecutor(new Commands());
+	}
 
 }
