6dd936f3f1a72dd2c361af9e31e7d74add6ed073
Jan Bukkit <info@mine-home.net>
2016-04-07 02:47:56 +0200

    Game-Class Speicherung des namens Config und Plugin initialisiert Einige Grundgerüste für plugin.yml in den Src-Ordner verschoben VIPHide hinzugefügt

--- src/me/joel/elytrapvp/Game.java
+++ src/me/joel/elytrapvp/Game.java
@@ -2,12 +2,72 @@ package me.joel.elytrapvp;
 
 import java.util.ArrayList;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.joel.elytrapvp.utils.Config;
+import me.joel.elytrapvp.utils.SimpleConfig;
 
 public class Game {
 
+	final private String name;
+	private SimpleConfig cfg;
+	private JavaPlugin plugin;
+
 	public Game(String name) {
+		this.name = name;
+		this.plugin = Main.plugin;
+		this.cfg = Config.getData();
 		player = new ArrayList<>();
+	}
+
+	public void join(Player p) {
+		//TODO Spieler tritt dem Spiel bei
+	}
+	
+	public void giveLobbyItems(Player p){
+		//TODO Gibt dem Spieler alle Lobby-Items
+	}
+
+	public void leave(Player p) {
+		//TODO Spieler verlässt das Spiel
+	}
+
+	public void stop(){
+		//TODO Stoppt das Spiel
+	}
+	
+	public void reset(){
+		//TODO Setzt alle ArrayListen und geschossene Projektile zurück
+	}
+	
+	public void dead(Player p) {
+		//TODO wird aufgerufen, wenn ein Spieler stirbt
+	}
+	
+	public void setHolo(Location loc){
+		setLocation(loc, "holo");
+	}
+	
+	public Location getHolo(){
+		return getLocation("holo");
+	}
+	
+	public void setLobby(Location loc) {
+		setLocation(loc, "lobby");
+	}
+	
+	public Location getLobby(){
+		return getLocation("lobby");
+	}
+
+	public void setLocation(Location loc, String key) {
+		// TODO Setzen der Location in der cfg
+	}
+
+	public Location getLocation(String key) {
+		return null; // TODO Holen der Location aus der cfg
 	}
 
 	public static ArrayList<Player> player;
